40be760edacac36a728516c9f062fe45b01ebf7a
jukangpark <skyxxx9339@naver.com>
2021-12-28 12:16:48 +0900

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -68,6 +69,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 };
